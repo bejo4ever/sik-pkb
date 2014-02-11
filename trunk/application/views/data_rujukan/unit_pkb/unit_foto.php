625a4406a68cd59da3a1f26c7485b1be5c823f62
angginurfa@gmail.com <angginurfa@gmail.com@f626c2cc-5b33-ec76-160a-bb06eabd18d8>
2014-02-11 04:45:44 +0000

    reupload

--- trunk/application/views/data_rujukan/unit_pkb/unit_foto.php
+++
@@ -1,8 +0,0 @@
-<html>
-<?php foreach($unit_pkb as $d): ?>
-<div align="center">
-
-<img style="height:500px" src="<?= base_url(); ?>uploads/unit/<?= $d->foto; ?>"/>
-<?php endforeach; ?>
-</div>
-</html>
